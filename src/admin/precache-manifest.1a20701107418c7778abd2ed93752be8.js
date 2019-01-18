56c3f29e4b775d5d53d9e855e05461163dfe8302
Klemen Kenda <klemen.kenda@qlector.com>
2019-01-18 16:37:53 +0100

    Adding built version of admin-react

---
+++ src/admin/precache-manifest.1a20701107418c7778abd2ed93752be8.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "3f82f5bf8566dc34c9b5",
+    "url": "/admin/static/js/runtime~main.3f82f5bf.js"
+  },
+  {
+    "revision": "0fa021755d273ef36a51",
+    "url": "/admin/static/js/main.0fa02175.chunk.js"
+  },
+  {
+    "revision": "c5b9ffbb7351681d91ba",
+    "url": "/admin/static/js/1.c5b9ffbb.chunk.js"
+  },
+  {
+    "revision": "0fa021755d273ef36a51",
+    "url": "/admin/static/css/main.56f8ea60.chunk.css"
+  },
+  {
+    "revision": "55322171c756d069002e43dce719ef3e",
+    "url": "/admin/index.html"
+  }
+];
